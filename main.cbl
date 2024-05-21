a9bc1d354f5a547349f54b5e053b59a1a4d02ddd
lexara-prime-ai <irfanghat@gmail.com>
2024-05-21 13:04:53 +0300

    Added source files

---
+++ main.cbl
@@ -0,0 +1,17 @@
+      *****************************************************
+      * Author: Irfan Ghat
+      * Date: May 21, 2024
+      * Purposr: Display text
+      * Tectonics: cobc
+      ******************************************************
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. PROJECT-SETUP.
+       ENVIRONMENT DIVISION.
+       DATA DIVISION.
+       FILE SECTION.
+       WORKING-STORAGE SECTION.
+       PROCEDURE DIVISION.
+       MAIN-PROCEDURE.
+           DISPLAY "Compilation Successful!"
+           STOP RUN.
+       END PROGRAM PROJECT-SETUP.
